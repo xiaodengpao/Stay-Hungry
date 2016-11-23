d21b9fdafd77029ae5f6e4de53b793b5149e999d
邢东朋 <623052434@qq.com>
2016-11-23 10:38:45 +0800

    Vue超级简化版

--- src/js/Vue的简单实现/index.js
+++ src/js/Vue的简单实现/index.js
@@ -23,6 +23,82 @@ class EventEmitter {
 
 /**
 * @ author: xingdongpeng
+* @ name: Vue的超级简化版
+* @ date: 2016-11-23
+*/
+class Observe {
+	constructor(data) {
+		this._data = data;
+		this.init();
+	}
+
+	init() {
+		for ( let item in this._data ){
+			// ES6的块级作用于 LET声明创建多个Dep实例
+			let dep = new Dep();
+			if ( this._data.hasOwnProperty(item) ){
+				Object.defineProperty( this, item, {
+					set: val=> {
+						if (this._data[item] !== val){
+							this._data[item] = val;
+							dep.trigger();
+						}
+					},
+					// watch的时候会调用get，将watcher对象推入栈中
+					get: ()=> {
+						if( Dep.target ) {
+							dep.push( Dep.target )
+							Dep.target = null;
+						}
+						return this._data[item];
+					}
+				} );
+			}
+		}	
+	}
+}
+
+class Dep {
+	constructor() {
+		this.sub = [];
+	}
+	trigger() {
+		this.sub.forEach((...arg) => {
+			
+			arg[0].update();
+		})
+	}
+	push(watcher) {
+		this.sub.push(watcher);
+	}
+}
+
+class Watcher {
+	constructor( exp, vm, func) {
+		this.func = func;
+		
+		// 把watcher扔进Dep.target指针
+		Dep.target = this;
+
+		// 读取Observe的a变量，调用get
+		vm[exp];
+	}
+	update(...arg) {
+		this.func.apply(  null, ...arg );
+	}
+}
+
+let ob = new Observe({a:1,b:2});
+
+new Watcher( 'a', ob, function(){console.log(this)} )
+ob.a = 3;
+
+
+
+
+
+/**
+* @ author: xingdongpeng
 * @ name: observe
 * @ date: 2016-11-11
 * @ desc: 这个类实现的目的是为了代理JSON对象的key，new Observer() 生产的类的实例是我们需要的响应式数据
@@ -456,3 +532,9 @@ list.__proto__ = arrayAugmentations;
 // 	methods(...args){
 // 	}
 // }
+
+
+
+
+
+
--- src/js/测试/index.js
+++ src/js/测试/index.js
@@ -1,4 +1,3 @@
-
 class Observe {
 	constructor(data) {
 		this._data = data;
@@ -7,19 +6,27 @@ class Observe {
 
 	init() {
 		for ( let item in this._data ){
+			// ES6的块级作用于 LET声明创建多个Dep实例
+			let dep = new Dep();
 			if ( this._data.hasOwnProperty(item) ){
-				// ES6的块级作用于 LET声明
 				Object.defineProperty( this, item, {
 					set: val=> {
-						this._data.item = val;
+						if (this._data[item] !== val){
+							this._data[item] = val;
+							dep.trigger();
+						}
 					},
+					// watch的时候会调用get，将watcher对象推入栈中
 					get: ()=> {
+						if( Dep.target ) {
+							dep.push( Dep.target )
+							Dep.target = null;
+						}
 						return this._data[item];
 					}
 				} );
 			}
-		}
-		
+		}	
 	}
 }
 
@@ -29,6 +36,7 @@ class Dep {
 	}
 	trigger() {
 		this.sub.forEach((...arg) => {
+			
 			arg[0].update();
 		})
 	}
@@ -38,12 +46,24 @@ class Dep {
 }
 
 class Watcher {
-	constructor() {
+	constructor( exp, vm, func) {
+		this.func = func;
+		
+		// 把watcher扔进Dep.target指针
+		Dep.target = this;
 
+		// 读取Observe的a变量，调用get
+		vm[exp];
+	}
+	update(...arg) {
+		this.func.apply(  null, ...arg );
 	}
 }
 
 let ob = new Observe({a:1,b:2});
-console.log(ob)
+
+new Watcher( 'a', ob, function(){console.log(this)} )
+ob.a = 3;
+
 
 
